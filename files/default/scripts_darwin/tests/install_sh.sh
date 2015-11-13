1d8a16aab0d298a517cea3fb4f72788fa37517c8
John Keiser <john@johnkeiser.com>
2015-11-13 09:17:22 -0800

    Fix up install.sh test for darwin

---
+++ files/default/scripts_darwin/tests/install_sh.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+run_cmd rm -rf /opt/chef
+
+echo "Grabbing install.sh ..."
+run_cmd timeout 60 curl https://omnitruck.chef.io/install.sh > /tmp/install.sh && {
+  run_cmd timeout 300 sudo -E bash /tmp/install.sh
+} || PROXY_TEST_RESULT=failed
