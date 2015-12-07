a8e3c43ccc0488d5b88ba8fb1879a494425b50eb
Nathan L Smith <smith@chef.io>
2015-12-07 15:46:50 -0600

    Add upload test for berkshelf.

--- recipes/render.rb
+++ recipes/render.rb
@@ -6,3 +6,13 @@ if node[:os] == "darwin"
     source "scripts_#{node[:os]}"
   end
 end
+
+# Berkshelf needs a config.json with an absolute path to the client key. Write
+# it out.
+file "#{ENV['PROXY_TESTS_DIR']}/tests/berkshelf/config.json" do
+  content JSONCompat.to_json_pretty "chef" => {
+    "chef_server_url" => "https://api.chef.io/organizations/chef_proxy_tests",
+    "client_key" => "#{ENV['PROXY_TESTS_REPO']}/.chef/client-creator.pem",
+    "node_name" => "client-creator",
+  }
+end
