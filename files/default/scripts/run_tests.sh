959d91c22e05cce996c4f9f8205aa363214d10a7
tyler-ball <tyleraball@gmail.com>
2015-12-10 09:17:33 -0700

    This echo line is polluting the logs

--- files/default/scripts/run_tests.sh
+++ files/default/scripts/run_tests.sh
@@ -14,7 +14,7 @@ echo "Running tests from $PROXY_TESTS_DIR ..."
 . $PROXY_TESTS_DIR/utils.sh
 
 test_is_pending() {
-  echo "test_is_pending $1 $2 $3"
+  #echo "test_is_pending $1 $2 $3"
   [[ $1 == "berkshelf" || ( $1 == "chef_client" && $3 == "no_proxy" ) || ( $1 == "install_sh" && $3 == "env_upper") || ( $1 == "chef_client" && $2 == "single" && $3 == "env_upper" ) || ( $1 != "chef_client" && $3 == "client_rb" ) ]]
 }
 
