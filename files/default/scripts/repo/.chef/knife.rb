27e85e3689f7eb8ada36691a47b8ad5ea336f37f
Nathan L Smith <smith@chef.io>
2015-12-30 16:55:18 -0600

    use domains without dots for no_proxy

--- files/default/scripts/repo/.chef/knife.rb
+++ files/default/scripts/repo/.chef/knife.rb
@@ -1,6 +1,6 @@
 chef_repo_path           File.expand_path("../..", __FILE__)
 chef_server_url          "https://api.chef.io/organizations/chef_proxy_tests"
-#node_name                "jkeiser"
+node_name                "jkeiser"
 #client_key               File.expand_path("~/.chef/keys/jkeiser.pem") if File.exist?(File.expand_path("~/.chef/keys/jkeiser.pem"))
 
 # Adding a method that handles setting the chef config for proxies.
