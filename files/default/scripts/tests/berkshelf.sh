9f70de3d109dd077447a4f2672c3ac2cd1ec29ca
tyler-ball <tyleraball@gmail.com>
2016-01-15 11:32:25 -0700

    Berkshelf needs to have the chef config file in a known place since we cannot specify it anymore

--- files/default/scripts/tests/berkshelf.sh
+++ files/default/scripts/tests/berkshelf.sh
@@ -5,6 +5,9 @@ BERKSHELF_TESTS_DIR=$PROXY_TESTS_DIR/tests/berkshelf
 # Clear the berkshelf
 run_cmd rm -rf ~/.berkshelf
 
+# Lets make sure the chef config can be read - put it in the user's home dir
+run_cmd cp -r $PROXY_TESTS_REPO/.chef ~/
+
 # Attempt a berks install using the sample Berksfile
 run_cmd berks install --berksfile=$BERKSHELF_TESTS_DIR/Berksfile || PROXY_TEST_RESULT=failed
 
@@ -16,3 +19,6 @@ run_cmd berks install --berksfile=$BERKSHELF_TESTS_DIR/Berksfile || PROXY_TEST_R
 # response. If we get this response it means we connected successfully through
 # the proxy.
 run_cmd berks install --berksfile=$BERKSHELF_TESTS_DIR/Berksfile && berks upload --config=$BERKSHELF_TESTS_DIR/config.json --berksfile=$BERKSHELF_TESTS_DIR/Berksfile | grep HTTPForbidden || PROXY_TEST_RESULT=failed
+
+# clean up the home user chef folder so it doesn't conflict with other tests
+run_cmd rm -rf ~/.chef
