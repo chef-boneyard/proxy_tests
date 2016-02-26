45774a3ddee0d3d2e46486e1c57abde30938516a
Chris Doherty <cdoherty@chef.io>
2016-02-25 16:36:04 -0800

    knife.rb: remove snowflake environment settings.

--- files/default/scripts/repo/.chef/knife.rb
+++ files/default/scripts/repo/.chef/knife.rb
@@ -1,7 +1,7 @@
 chef_repo_path           File.expand_path("../..", __FILE__)
 chef_server_url          "https://api.chef.io/organizations/chef_proxy_tests"
-#node_name                "jkeiser"
-#client_key               File.expand_path("~/.chef/keys/jkeiser.pem") if File.exist?(File.expand_path("~/.chef/keys/jkeiser.pem"))
+node_name                "client-creator"
+client_key               "client-creator.pem"
 
 # Adding a method that handles setting the chef config for proxies.
 # Then single tests or configurations can call this if they want to
